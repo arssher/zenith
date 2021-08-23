547bd7835a393352e280216b26f0548c9f64cdcc
Patrick Insinger <patrickinsinger@gmail.com>
2021-08-23 13:42:33 -0700

    snapshot_layer - read page versions from disk
    split the page versions into two chapters:
    PAGE_VERSIONS_META - a rust BTreeMap from (block #, lsn) -> page & WAL
    byte ranges in PAGE_VERSIONS_CHAPTER
    PAGE_VERSIONS_CHAPTER - raw page images and serialized WAL records

--- pageserver/src/layered_repository/snapshot_layer.rs
+++ pageserver/src/layered_repository/snapshot_layer.rs
@@ -14,8 +14,10 @@
 //! timeline+LSN, and once you've located it, you have all the data you need to in that
 //! file.
 //!
-//! When a snapshot file needs to be accessed, we slurp the whole file into memory, into
-//! the SnapshotLayer struct. See load() and unload() functions.
+//! When a snapshot file needs to be accessed, we read the relsizes and page version
+//! metadata into the SnapshotLayer struct. See load() and unload() functions.
+//! To materialize a page, we iterate backwards through the metadata, reading
+//! any WAL records and a base page image directly from the file.
 //!
 //! On disk, the snapshot files are stored in timelines/<timelineid> directory.
 //! Currently, there are no subdirectories, and each snapshot file is named like this:
@@ -34,17 +36,21 @@
 //! The end LSN indicates when it was dropped in that case, we don't store it in the
 //! file contents in any way.
 //!
-//! A snapshot file is constructed using the 'bookfile' crate. Each file consists of two
-//! parts: the page versions and the relation sizes. They are stored as separate chapters.
+//! A snapshot file is constructed using the 'bookfile' crate. Each file consists of three
+//! parts: the page versions, a mapping of block number and LSN to page version byte ranges,
+//! and the relation sizes. They are stored as separate chapters.
 //!
 use crate::layered_repository::storage_layer::{
     Layer, PageReconstructData, PageVersion, SegmentTag,
 };
 use crate::relish::*;
+use crate::repository::WALRecord;
 use crate::PageServerConf;
 use crate::{ZTenantId, ZTimelineId};
 use anyhow::{bail, Result};
+use bytes::Bytes;
 use log::*;
+use serde::{Deserialize, Serialize};
 use std::collections::BTreeMap;
 use std::fmt;
 use std::fs;
@@ -54,7 +60,7 @@ use std::ops::Bound::Included;
 use std::path::PathBuf;
 use std::sync::{Arc, Mutex, MutexGuard};
 
-use bookfile::{Book, BookWriter};
+use bookfile::{Book, BookWriter, BoundedReader, ChapterId, ChapterWriter};
 
 use zenith_utils::bin_ser::BeSer;
 use zenith_utils::lsn::Lsn;
@@ -62,8 +68,57 @@ use zenith_utils::lsn::Lsn;
 // Magic constant to identify a Zenith snapshot file
 static SNAPSHOT_FILE_MAGIC: u32 = 0x5A616E01;
 
-static PAGE_VERSIONS_CHAPTER: u64 = 1;
+/// Mapping from block number and LSN to page/WAL byte ranges into PAGE_VERSIONS_CHAPTER
+static PAGE_VERSIONS_META_CHAPTER: u64 = 1;
 static REL_SIZES_CHAPTER: u64 = 2;
+/// Raw page images and WAL records, can only be interpreted with byte ranges from PAGE_VERSIONS_META_CHAPTER
+static PAGE_VERSIONS_CHAPTER: u64 = 3;
+
+#[derive(Serialize, Deserialize)]
+struct BlobRange {
+    offset: u64,
+    size: usize,
+}
+
+#[derive(Serialize, Deserialize)]
+struct PageVersionMeta {
+    page_image_range: Option<BlobRange>,
+    record_range: Option<BlobRange>,
+}
+
+fn read_blob(reader: &BoundedReader<&'_ File>, range: &BlobRange) -> Result<Vec<u8>> {
+    let mut buf = vec![0u8; range.size];
+    reader.read_exact_at(&mut buf, range.offset)?;
+    Ok(buf)
+}
+
+struct BlobWriter {
+    writer: ChapterWriter<File>,
+    offset: u64,
+}
+
+impl BlobWriter {
+    // This function takes a BookWriter and creates a new chapter to ensure offset is 0.
+    fn new(book_writer: BookWriter<File>, chapter_id: impl Into<ChapterId>) -> Self {
+        let writer = book_writer.new_chapter(chapter_id);
+        Self { writer, offset: 0 }
+    }
+
+    fn write_blob(&mut self, blob: &[u8]) -> Result<BlobRange> {
+        self.writer.write_all(blob)?;
+
+        let range = BlobRange {
+            offset: self.offset,
+            size: blob.len(),
+        };
+        self.offset += blob.len() as u64;
+        Ok(range)
+    }
+
+    fn close(self) -> bookfile::Result<BookWriter<File>> {
+        self.writer.close()
+    }
+}
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
 struct SnapshotFileName {
@@ -230,13 +285,13 @@ pub struct SnapshotLayer {
 }
 
 pub struct SnapshotLayerInner {
-    /// If false, the 'page_versions' and 'relsizes' have not been
+    /// If false, the 'page_version_meta' and 'relsizes' have not been
     /// loaded into memory yet.
     loaded: bool,
 
-    /// All versions of all pages in the file are are kept here.
+    /// Offsets and sizes of all page versions on disk.
     /// Indexed by block number and LSN.
-    page_versions: BTreeMap<(u32, Lsn), PageVersion>,
+    page_version_meta: BTreeMap<(u32, Lsn), PageVersionMeta>,
 
     /// `relsizes` tracks the size of the relation at different points in time.
     relsizes: BTreeMap<Lsn, u32>,
@@ -272,21 +327,31 @@ impl Layer for SnapshotLayer {
     ) -> Result<Option<Lsn>> {
         // Scan the BTreeMap backwards, starting from the given entry.
         let mut need_base_image_lsn: Option<Lsn> = Some(lsn);
+
+        // TODO: avoid opening the snapshot file for each read
+        let (_path, book) = self.open_book()?;
+        let page_version_reader = book.chapter_reader(PAGE_VERSIONS_CHAPTER)?;
+
         {
             let inner = self.load()?;
+
             let minkey = (blknum, Lsn(0));
             let maxkey = (blknum, lsn);
             let mut iter = inner
-                .page_versions
+                .page_version_meta
                 .range((Included(&minkey), Included(&maxkey)));
             while let Some(((_blknum, entry_lsn), entry)) = iter.next_back() {
-                if let Some(img) = &entry.page_image {
-                    reconstruct_data.page_img = Some(img.clone());
+                if let Some(img_range) = &entry.page_image_range {
+                    let img = Bytes::from(read_blob(&page_version_reader, img_range)?);
+
+                    reconstruct_data.page_img = Some(img);
                     need_base_image_lsn = None;
                     break;
-                } else if let Some(rec) = &entry.record {
-                    reconstruct_data.records.push(rec.clone());
-                    if rec.will_init {
+                } else if let Some(rec_range) = &entry.record_range {
+                    let rec = WALRecord::des(&read_blob(&page_version_reader, rec_range)?)?;
+                    let will_init = rec.will_init;
+                    reconstruct_data.records.push(rec);
+                    if will_init {
                         // This WAL record initializes the page, so no need to go further back
                         need_base_image_lsn = None;
                         break;
@@ -384,20 +449,20 @@ impl SnapshotLayer {
         relsizes: BTreeMap<Lsn, u32>,
     ) -> Result<SnapshotLayer> {
         let snapfile = SnapshotLayer {
-            conf: conf,
-            timelineid: timelineid,
-            tenantid: tenantid,
-            seg: seg,
-            start_lsn: start_lsn,
+            conf,
+            timelineid,
+            tenantid,
+            seg,
+            start_lsn,
             end_lsn,
             dropped,
             inner: Mutex::new(SnapshotLayerInner {
                 loaded: true,
-                page_versions: page_versions,
-                relsizes: relsizes,
+                page_version_meta: BTreeMap::new(),
+                relsizes,
             }),
         };
-        let inner = snapfile.inner.lock().unwrap();
+        let mut inner = snapfile.inner.lock().unwrap();
 
         // Write the in-memory btreemaps into a file
         let path = snapfile.path();
@@ -407,9 +472,38 @@ impl SnapshotLayer {
         let file = File::create(&path)?;
         let book = BookWriter::new(file, SNAPSHOT_FILE_MAGIC)?;
 
+        let mut page_version_writer = BlobWriter::new(book, PAGE_VERSIONS_CHAPTER);
+
+        for (key, page_version) in page_versions {
+            let page_image_range = page_version
+                .page_image
+                .map(|page_image| page_version_writer.write_blob(page_image.as_ref()))
+                .transpose()?;
+
+            let record_range = page_version
+                .record
+                .map(|record| {
+                    let buf = WALRecord::ser(&record)?;
+                    page_version_writer.write_blob(&buf)
+                })
+                .transpose()?;
+
+            let old = inner.page_version_meta.insert(
+                key,
+                PageVersionMeta {
+                    page_image_range,
+                    record_range,
+                },
+            );
+
+            assert!(old.is_none());
+        }
+
+        let book = page_version_writer.close()?;
+
         // Write out page versions
-        let mut chapter = book.new_chapter(PAGE_VERSIONS_CHAPTER);
-        let buf = BTreeMap::ser(&inner.page_versions)?;
+        let mut chapter = book.new_chapter(PAGE_VERSIONS_META_CHAPTER);
+        let buf = BTreeMap::ser(&inner.page_version_meta)?;
         chapter.write_all(&buf)?;
         let book = chapter.close()?;
 
@@ -428,17 +522,7 @@ impl SnapshotLayer {
         Ok(snapfile)
     }
 
-    ///
-    /// Load the contents of the file into memory
-    ///
-    fn load(&self) -> Result<MutexGuard<SnapshotLayerInner>> {
-        // quick exit if already loaded
-        let mut inner = self.inner.lock().unwrap();
-
-        if inner.loaded {
-            return Ok(inner);
-        }
-
+    fn open_book(&self) -> Result<(PathBuf, Book<File>)> {
         let path = Self::path_for(
             self.conf,
             self.timelineid,
@@ -454,8 +538,24 @@ impl SnapshotLayer {
         let file = File::open(&path)?;
         let book = Book::new(file)?;
 
-        let chapter = book.read_chapter(PAGE_VERSIONS_CHAPTER)?;
-        let page_versions = BTreeMap::des(&chapter)?;
+        Ok((path, book))
+    }
+
+    ///
+    /// Load the page version metadata and relsizes into memory
+    ///
+    fn load(&self) -> Result<MutexGuard<SnapshotLayerInner>> {
+        // quick exit if already loaded
+        let mut inner = self.inner.lock().unwrap();
+
+        if inner.loaded {
+            return Ok(inner);
+        }
+
+        let (path, book) = self.open_book()?;
+
+        let chapter = book.read_chapter(PAGE_VERSIONS_META_CHAPTER)?;
+        let page_version_meta = BTreeMap::des(&chapter)?;
 
         let chapter = book.read_chapter(REL_SIZES_CHAPTER)?;
         let relsizes = BTreeMap::des(&chapter)?;
@@ -464,7 +564,7 @@ impl SnapshotLayer {
 
         *inner = SnapshotLayerInner {
             loaded: true,
-            page_versions,
+            page_version_meta,
             relsizes,
         };
 
@@ -497,7 +597,7 @@ impl SnapshotLayer {
                     dropped: snapfilename.dropped,
                     inner: Mutex::new(SnapshotLayerInner {
                         loaded: false,
-                        page_versions: BTreeMap::new(),
+                        page_version_meta: BTreeMap::new(),
                         relsizes: BTreeMap::new(),
                     }),
                 };
@@ -520,7 +620,7 @@ impl SnapshotLayer {
     ///
     pub fn unload(&self) -> Result<()> {
         let mut inner = self.inner.lock().unwrap();
-        inner.page_versions = BTreeMap::new();
+        inner.page_version_meta = BTreeMap::new();
         inner.relsizes = BTreeMap::new();
         inner.loaded = false;
         Ok(())
